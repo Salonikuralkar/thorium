f89923069765443cbfc11d7f04576a516f6bf0b1
Salonikuralkar <saloni.kuralkar@gmail.com>
2022-03-01 21:17:13 +0530

    BookAndAuthor_Schema

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,59 +1,60 @@
 const { count } = require("console")
 const BookModel= require("../models/bookModel")
+const AuthorModel =require("../models/authorModel")
 
-const createBook= async function (req, res) {
+const createAuthors= async function (req, res) {
     let data= req.body
-
+    let savedData= await AuthorModel.create(data)
+    res.send({msg: savedData})
+}////////////////////////////////////////////////////////author create
+const createBooks= async function (req, res) {
+    let data= req.body
     let savedData= await BookModel.create(data)
     res.send({msg: savedData})
+}/////////////////////////////////////////////////////////book create
+
+const getAuthorID= async function (req, res) {
+    let author= await AuthorModel.find( {authorName : "Chetan Bhagat" }).select( { author_id:1,_id: 0});
+   let id=author[0].author_id;
+    let bookChetan=await BookModel.find({author_id:id}).select({bookName:1,_id:0})
+    res.send({msg: bookChetan})///////////////////////////////////finding books of chetan bhagat
+}
+const getAuthorUpdatePrice= async function (req, res) {
+    let data= await BookModel.findOneAndUpdate( 
+        {bookName : "Two States"},
+        {$set :{price:100}},
+        {new: true});
+        let id=data.author_id;        
+        let cost=data.price;
+       // res.send({id})
+        let name=await AuthorModel.find({author_id:{$eq:id}}).select({authorName:1,_id:0})
+        res.send({msg: name,cost})///////////////////////////////////finding bookname and update price
 }
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find( {authorName : "HO" } )
-    console.log(allBooks)
-    if (allBooks.length > 0 )  res.send({msg: allBooks, condition: true})
-    else res.send({msg: "No books found" , condition: false})
+const getPrice= async function (req, res) {
+    let authInfo= await BookModel.find( { price : { $gte: 50, $lte: 100} } ).select({ author_id :1,_id:0});
+    // res.send({data})
+    let data=authInfo.map(input=>input.author_id)
+    let arr=[];
+    for(let i=0;i<data.length;i++){    
+        let a=data[i];
+        let result= await AuthorModel.find({author_id:a}).select({authorName:1,_id:0})
+        arr.push(result)
+    }
+    const author=arr.flat()
+    
+    res.send({msg: author})///////////////////////////////////finding author name
 }
+module.exports.createAuthors= createAuthors
+
+module.exports.createBooks= createBooks
+
+module.exports.getAuthorID= getAuthorID
+
+module.exports.getAuthorUpdatePrice= getAuthorUpdatePrice
 
 
-const updateBooks= async function (req, res) {
-    let data = req.body // {sales: "1200"}
-    // let allBooks= await BookModel.updateMany( 
-    //     { author: "SK"} , //condition
-    //     { $set: data } //update in data
-    //  )
-    let allBooks= await BookModel.findOneAndUpdate( 
-        { authorName: "ABC"} , //condition
-        { $set: data }, //update in data
-        { new: true , upsert: true} ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT  
-     )
-     
-     res.send( { msg: allBooks})
-}
-
-const deleteBooks= async function (req, res) {
-    // let data = req.body 
-    let allBooks= await BookModel.updateMany( 
-        { authorName: "FI"} , //condition
-        { $set: {isDeleted: true} }, //update in data
-        { new: true } ,
-     )
-     
-     res.send( { msg: allBooks})
-}
+module.exports.getPrice= getPrice
 
 
 
-
-// CRUD OPERATIONS:
-// CREATE
-// READ
-// UPDATE
-// DELETE
-
-
-
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.updateBooks= updateBooks
-module.exports.deleteBooks= deleteBooks
--- src/index.js
+++ src/index.js
@@ -6,16 +6,12 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://SaloniKuralkar:Saloni%4030@cluster0.ui8kc.mongodb.net/Saloni_Kuralkar-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 app.use('/', route);
-
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
