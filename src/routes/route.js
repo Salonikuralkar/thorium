33e3b2fa888ac5970465bd38294d80fcd09f8005
Salonikuralkar <saloni.kuralkar@gmail.com>
2022-03-15 09:35:58 +0530

    Project Phase1

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+const AuthorController=require("../controller/authorController")
+const BlogsController=require("../controller/blogController")
 
 
+router.post("/authors",AuthorController.createAuthor )
+
+router.post("/blogs",BlogsController.createBlogs )
+
+router.get("/blogs",BlogsController.getBlogs)
+
+router.put("/blogs/:blogId", BlogsController.updateBlogs)
+
+router.delete("/blogs/:blogId", BlogsController.deleteBlogById)
+
+router.delete("/blogs",BlogsController.deleteBlogByQueryParams)
 
 
 module.exports = router;
